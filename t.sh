a1f784fca7234197467102dff85e695b5990444b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2017-06-02 06:27:36 +0900

    cache into individual files

--- t.sh
+++ t.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -n "$1" ]; then
-    unbuffer ./t_sub.sh $1 | tee cache | less -R;
+    unbuffer ./t_sub.sh $1 | tee `date +"cache/%Y%m%d_$1"` | less -R;
 else
     echo "Need a keyword to search";
 fi
